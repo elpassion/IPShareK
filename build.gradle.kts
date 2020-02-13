e789882046da08b1227529aee1c9ada9b289dbfd
Marek Langiewicz <marek.langiewicz@gmail.com>
2020-02-13 11:31:01 +0100

    Initial commit with example action

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.16"
+    kotlin("jvm") version "1.3.70-eap-42"
+}
+
+group = "pl.elpassion"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.3.2"
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
---
+++ src/main/kotlin/ExampleAction.kt
@@ -0,0 +1,10 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+class ExampleAction: AnAction("Example action") {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project!!
+        Messages.showMessageDialog(project, "Hello from ELPassion", "Example title", Messages.getInformationIcon())
+    }
+}
